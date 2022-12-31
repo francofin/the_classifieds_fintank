9649c2137b668592ace049f19244b6f758dc9adb
fintank <finapptank@gmail.com>
2022-12-31 01:30:52 -0500

    Added backend herokup api

--- fintank/next.config.js
+++ fintank/next.config.js
@@ -36,7 +36,8 @@ module.exports = withPlugins(
     },
     env: {
       production_type: "server", // Change variable to "static" for STATIC EXPORT
-      FINTANK_API_URL:'http://localhost:8000',
+      // FINTANK_API_URL:'http://localhost:8000',
+      FINTANK_API_URL:"https://fintank.herokuapp.com/",
       MAPBOX_ACCESS_TOKEN:'pk.eyJ1IjoiZnJhbmNvZmluIiwiYSI6ImNsNm9nNmIzYzBhbnIzaXRlNTJranJwankifQ.8EBtyLcfgqaQYIWn3y1pbA'
     },
     reactStrictMode: true,
