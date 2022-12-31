a665f9f9d8433a54c988e423f12ee1b1606e9b8a
fintank <finapptank@gmail.com>
2022-12-31 02:07:00 -0500

    This is when we grind

--- fintank/next.config.js
+++ fintank/next.config.js
@@ -37,7 +37,7 @@ module.exports = withPlugins(
     env: {
       production_type: "server", // Change variable to "static" for STATIC EXPORT
       // FINTANK_API_URL:'http://localhost:8000',
-      FINTANK_API_URL:"https://fintank.herokuapp.com/",
+      FINTANK_API_URL:"https://fintank.herokuapp.com",
       MAPBOX_ACCESS_TOKEN:'pk.eyJ1IjoiZnJhbmNvZmluIiwiYSI6ImNsNm9nNmIzYzBhbnIzaXRlNTJranJwankifQ.8EBtyLcfgqaQYIWn3y1pbA'
     },
     reactStrictMode: true,
