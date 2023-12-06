d644089afdd4aa131fc60c11d0b7e1cf6a92a4b9
fintank <finapptank@gmail.com>
2023-12-06 08:30:03 -0500

    put commodity page back still inaccessible

--- fintank/pages/commodity.js
+++ fintank/pages/commodity.js
@@ -1,143 +1,143 @@
-// import React, {useEffect, useState} from "react"
-// import Link from "next/link"
-// import Select from "react-select"
-// import sector from "@data/sectors-subsectors.json"
-// import {Container, Button, Form, InputGroup, Row, Col, Badge, Breadcrumb, Modal} from "react-bootstrap"
-// import universe from "@data/universe.json"
-// import axios from 'axios';
-// import { useRouter } from "next/router"
-// import {connect} from 'react-redux';
-// import screenerAction from '@src/actions/screenerAction'
-// import { bindActionCreators } from "redux"
-// import CommodListPagination from "@components/CommodPagination"
+import React, {useEffect, useState} from "react"
+import Link from "next/link"
+import Select from "react-select"
+import sector from "@data/sectors-subsectors.json"
+import {Container, Button, Form, InputGroup, Row, Col, Badge, Breadcrumb, Modal} from "react-bootstrap"
+import universe from "@data/universe.json"
+import axios from 'axios';
+import { useRouter } from "next/router"
+import {connect} from 'react-redux';
+import screenerAction from '@src/actions/screenerAction'
+import { bindActionCreators } from "redux"
+import CommodListPagination from "@components/CommodPagination"
 
 
-// export function getAllPostIds() {
-//   return universe.posts.map((post) => ({
-//     params: {
-//       slug: post.slug,
-//     },
-//   }))
-// }
+export function getAllPostIds() {
+  return universe.posts.map((post) => ({
+    params: {
+      slug: post.slug,
+    },
+  }))
+}
   
-// export async function getPostData(slug) {
-//   for (var i = 0; i < universe.posts.length; i++) {
-//     if (universe.posts[i].slug == slug) {
-//       return {
-//         stockItem: universe.posts[i],
-//         slug
-//       }
-//     }
-//   }
-// }a
+export async function getPostData(slug) {
+  for (var i = 0; i < universe.posts.length; i++) {
+    if (universe.posts[i].slug == slug) {
+      return {
+        stockItem: universe.posts[i],
+        slug
+      }
+    }
+  }
+}a
 
-// export async function getServerSideProps({ params, query }) {
-//   // const parameters= getAllPostIds()
+export async function getServerSideProps({ params, query }) {
+  // const parameters= getAllPostIds()
 
-// let res = await axios.get(`${process.env.NEXT_PUBLIC_FINTANK_API_URL}/universe-data/commod`);
-// let data = res.data;
+let res = await axios.get(`${process.env.NEXT_PUBLIC_FINTANK_API_URL}/universe-data/commod`);
+let data = res.data;
 
-// const totalStocks = data.count
-// const resultsPerPage = 20
+const totalStocks = data.count
+const resultsPerPage = 20
 
 
-// const numberOfPages = totalStocks/resultsPerPage
+const numberOfPages = totalStocks/resultsPerPage
 
-// return {
-//   props: {
-//     nav: {
-//       light: true,
-//       classes: "shadow",
-//       color: "white",
-//     },
-//     loggedUser: false,
-//     title: "Fintank Stock Screener",
-//     data,
-//     numberOfPages,
-//     totalStocks,
-//     resultsPerPage
-//   },
-// }
-// }
+return {
+  props: {
+    nav: {
+      light: true,
+      classes: "shadow",
+      color: "white",
+    },
+    loggedUser: false,
+    title: "Fintank Stock Screener",
+    data,
+    numberOfPages,
+    totalStocks,
+    resultsPerPage
+  },
+}
+}
 
-// const Universe = (props) => {
+const Universe = (props) => {
 
-//   const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false)
 
    
-//   const univerData = props.data.items
+  const univerData = props.data.items
   
 
-//   const [universe, setUniverse] = useState(props.data.items)
+  const [universe, setUniverse] = useState(props.data.items)
 
-//   // let sectorFilter = null
-//   const [sectorHookFilter, setSectorFilter] = useState('All');
-//   // let stockData = univerData;
-//   let filterData = univerData;
-//   const [numberOfStocks, setNumberOfStocks] = useState(props.totalStocks);
-//   const [limitIndex, setLimitIndex] = useState(props.resultsPerPage);
-
-
-
-//   const router = useRouter();
+  // let sectorFilter = null
+  const [sectorHookFilter, setSectorFilter] = useState('All');
+  // let stockData = univerData;
+  let filterData = univerData;
+  const [numberOfStocks, setNumberOfStocks] = useState(props.totalStocks);
+  const [limitIndex, setLimitIndex] = useState(props.resultsPerPage);
 
 
-//   let queryParams;
-//   if(typeof window !== 'undefined'){
-//     queryParams = new URLSearchParams(window.location.search);
-//   }
+
+  const router = useRouter();
 
 
-//   useEffect(() => {
-//     const setUniverseData = async() => {
-//         await setUniverse(univerData)
-//     }
+  let queryParams;
+  if(typeof window !== 'undefined'){
+    queryParams = new URLSearchParams(window.location.search);
+  }
 
-//     setUniverseData()
-//   }, [univerData])
+
+  useEffect(() => {
+    const setUniverseData = async() => {
+        await setUniverse(univerData)
+    }
+
+    setUniverseData()
+  }, [univerData])
   
 
   
 
-//   return (
-//     <section className="py-5">
-//       <Container>
-//         <Breadcrumb listProps={{ className: "ps-0 justify-content-center" }}>
-//           <Link href="/" passHref>
-//             <Breadcrumb.Item>Home</Breadcrumb.Item>
-//           </Link>
-//           <Breadcrumb.Item active>Host view</Breadcrumb.Item>
-//         </Breadcrumb>
-//         <div className="d-flex justify-content-between align-items-end mb-5">
-//           <h1 className="hero-heading mb-0">{`Commodities`}</h1>
-//         </div>
+  return (
+    <section className="py-5">
+      <Container>
+        <Breadcrumb listProps={{ className: "ps-0 justify-content-center" }}>
+          <Link href="/" passHref>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+          </Link>
+          <Breadcrumb.Item active>Host view</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="d-flex justify-content-between align-items-end mb-5">
+          <h1 className="hero-heading mb-0">{`Commodities`}</h1>
+        </div>
 
-//         <div className="d-flex justify-content-between align-items-center flex-column flex-lg-row mb-5 mt-5">
-//           <div className="me-3">
-//             <p className="mb-3 mb-lg-0">
-//               There are <strong>{props.data.count}</strong> Commodities <i>Some Commodities May not be listed</i>
-//             </p>
-//           </div>
-//         </div>
-//         <CommodListPagination dataProps={universe} itemsPerPage={limitIndex} />
+        <div className="d-flex justify-content-between align-items-center flex-column flex-lg-row mb-5 mt-5">
+          <div className="me-3">
+            <p className="mb-3 mb-lg-0">
+              There are <strong>{props.data.count}</strong> Commodities <i>Some Commodities May not be listed</i>
+            </p>
+          </div>
+        </div>
+        <CommodListPagination dataProps={universe} itemsPerPage={limitIndex} />
        
-//       </Container>
-//     </section>
-//   )
-// }
+      </Container>
+    </section>
+  )
+}
 
-// function mapStateToProps(state){
-//   return {
-//     screenerAction:state.screener
-//   }
-// }
+function mapStateToProps(state){
+  return {
+    screenerAction:state.screener
+  }
+}
 
-// function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch){
 
-//   return bindActionCreators({
-//     screenerActionRun:screenerAction
-//   }, dispatch)
+  return bindActionCreators({
+    screenerActionRun:screenerAction
+  }, dispatch)
 
-// }
+}
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Universe);
+export default connect(mapStateToProps, mapDispatchToProps)(Universe);
